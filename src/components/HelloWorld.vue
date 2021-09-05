43757f09a753ab93d71ee7b8c1a98b6e84a23f03
DanielArmengod <danielarmengodsorrosal@gmail.com>
2021-09-05 14:45:01 +0200

    Refactor y nuevo datetime input

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,64 +1,56 @@
 <template>
   <v-form v-model="valid">
     <v-container>
-      <v-row align="center"
-      justify="center">
-
-                    <img src="@/assets/img.svg" width="50%" height="50%"/>
+      <v-row align="center" justify="center">
+        <img src="@/assets/img.svg" width="50%" height="100%"/>
       </v-row>
-      <v-row align="center"
-      justify="center">
-        <v-col
-          cols="0"
-          md="0"
-        >
-          <v-text-field
-            v-model="title"
-            :rules="nameRules"
-            :counter="10"
-            label="Title of the reminder"
-            required
-          ></v-text-field>
+      <v-row align="center" justify="center">
+        <v-col cols="13" md="10">
+          <v-text-field v-model="title" :rules="nameRules" :counter="40" 
+            label="Title of the reminder" required></v-text-field>
         </v-col>
+      </v-row>
 
-        <v-col
-          cols="12"
-          md="10"
-        >
-
-                    <v-text-field
-            v-model="email"
-            :rules="emailRules"
-            label="E-mail"
-            aria-placeholder="write your reminder"
-            required
-          ></v-text-field>
+      <v-row align="center" justify="center">
+        <v-col cols="13" md="10">
+          <v-text-field v-model="email" :rules="emailRules" label="E-mail" 
+            aria-placeholder="write your reminder" required ></v-text-field>
         </v-col>
+      </v-row>
+      
+      <v-row align="center" justify="center">
+        <v-col cols="13" md="10">
+          <v-textarea v-model="content" name="input-7-1" filled label="Argument for the reminder ..." auto-grow required></v-textarea>
+        </v-col>
+      </v-row>
 
-        <v-col
-          cols="12"
-          md="4"
-        >
+      <v-row align="center" justify="center">
+        <v-col cols="13" md="10">
+          Scheduled date: <input required class="mr-4" type="datetime-local" v-model="datetimescheduled" name="datetime">
+        </v-col>
+      </v-row>
 
-              <v-textarea
-              v-model="content"
-      name="input-7-1"
-      filled
-      label="Label"
-      auto-grow
-      value="The Woodman set to work at once, and so sharp was his axe that the tree was soon chopped nearly through."
-      required
-    ></v-textarea>
+      <v-row align="center" justify="center">
+        <v-col cols="13" md="10">
+          <v-btn class="mr-4" @click="submit">SCHEDULE</v-btn>
         </v-col>
       </v-row>
     </v-container>
+    <!-- button for scroll to down -->
+    <v-btn v-scroll="onScroll" v-show="fab" width = "50%" dark fixed block elevation="24" bottom color="none" @click="toBottom">
+      <v-icon>mdi-arrow-down</v-icon>
+    </v-btn>
   </v-form>
+  
 </template>
 
 <script>
   export default {
     data: () => ({
+      dialog: false,
       valid: false,
+      fab: true,
+      datetimescheduled: null,
       title: '',
       content: '',
       nameRules: [
@@ -70,5 +62,15 @@
         v => /.+@.+/.test(v) || 'E-mail must be valid',
       ],
     }),
+    methods: {
+      onScroll (e) {
+        if (typeof window === 'undefined') return
+        const top = window.pageYOffset ||   e.target.scrollTop || 0
+        this.fab = top < 20
+      },
+      toBottom () {
+        this.$vuetify.goTo(document.body.scrollHeight)
+      }
+    },
   }
 </script>
